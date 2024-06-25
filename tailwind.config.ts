cd13c6ffbcc81486a8dd3ea863b77add9e73f323
kash2104 <kparikh1104@gmail.com>
2024-06-25 23:08:10 +0530

    config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontWeight: {
+      500: "500",
+      600: "600",
+    },
   },
   plugins: [],
 };
