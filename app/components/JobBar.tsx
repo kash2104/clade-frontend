cd3948dea484bd7ccdf46ffdf04f7eedb9af3ab2
kash2104 <kparikh1104@gmail.com>
2024-06-25 23:09:04 +0530

    NavBar

--- app/components/JobBar.tsx
+++ app/components/JobBar.tsx
@@ -1,55 +1,76 @@
-import React, { useState } from 'react'
-import { FiMessageSquare } from 'react-icons/fi'
-import { PiBagSimple, PiHandCoins } from 'react-icons/pi'
+import React, { useState } from "react";
+import { FiMessageSquare } from "react-icons/fi";
+import { PiBagSimple, PiHandCoins } from "react-icons/pi";
 
 const JobBar = () => {
-    const[active, setActive] = useState('jobs');
+  const [activeTab, setActiveTab] = useState("jobs");
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
   return (
     <>
-        <div className='w-[538px] h-[61px] border-[0.5px] rounded-[36px] border-[#D1D1D1] flex justify-around items-center'>
-            <div className={`w-[79px] h-[27px] top-[24px] left-[23px] gap-1 flex bg-[#DC4A2D] rounded-full p-5 justify-center items-center cursor-pointer`}>
-                <div className='w-6 h-6 flex justify-center items-center'>
-                    <div className=' w-5 h-[18px] top-[3px] left-[2px] text-[#FFFFFF]'>
-                        <PiBagSimple/>
-                    </div>
-                    
-                </div>
-
-                <div className='w-[47px] h-[27px] font-sans font-500 text-[20px] leading-[27px] text-[#FFFFFF]'>
-                    Jobs
-                </div>
+      <div className="w-[538px] h-[61px] border-[0.5px] rounded-[36px] border-[#D1D1D1] flex justify-around items-center">
+        <div
+          className={`w-[79px] h-[27px] top-[24px] left-[23px] gap-1 flex justify-center items-center p-5 cursor-pointer transition-all duration-200 ${
+            activeTab === "jobs"
+              ? "bg-[#DC4A2D] rounded-full text-[#FFFFFF] w-[109px]"
+              : "bg-transparent text-[#B0B0B0]"
+          }`}
+          onClick={() => handleTabClick("jobs")}
+        >
+          <div className="w-6 h-6 flex justify-center items-center">
+            <div className="w-5 h-[18px] top-[3px] left-[2px]">
+              <PiBagSimple />
             </div>
-
-            <div className='w-[128px] h-[27px] top-6 left-[182px] gap-1 flex cursor-pointer items-center justify-center'>
-                <div className='w-6 h-6 flex justify-center items-center relative'>
-                    <div className='w-[18px] h-[18.46px] top-[3px] left-[3px]text-[#B0B0B0]'>
-                        <FiMessageSquare/>
-                    </div>
-
-                    <div className='absolute w-[6px] h-[6px] text-[#DC4A2D] rounded-full top-0 right-0'>
-
-                    </div>
-                </div>
-
-                <div className='w-[96px] h-[27px] font-500 text-[20px] leading-[27px] text-[#B0B0B0]'>
-                    Messages
-                </div>
-            </div>
-
-            <div className=' w-[125px] h-[27px] top-[24px] left-[390px] gap-1 flex cursor-pointer items-center justify-center'>
-                <div className=' w-6 h-6 flex justify-center items-center'>
-                    <div className=' w-5 h-5 top-[2px] left-[2px]text-[#B0B0B0]'>
-                        <PiHandCoins/>
-                    </div>
-                </div>
-
-                <div className='text-[20px] leading-[27px] font-500 text-[#B0B0B0]'>
-                    Payments
-                </div>
-            </div>
+          </div>
+          <div
+            className={`w-[47px] h-[27px] font-sans font-500 text-[20px] leading-[27px]`}
+          >
+            Jobs
+          </div>
         </div>
-    </>
-  )
-}
 
-export default JobBar
+        <div
+          className={`w-[128px] h-[27px] top-6 left-[182px] gap-1 flex cursor-pointer items-center justify-center p-5 transition-all duration-200 ${
+            activeTab === "messages"
+              ? "bg-[#DC4A2D] rounded-full text-[#FFFFFF] w-[178px]"
+              : "bg-transparent text-[#B0B0B0]"
+          }`}
+          onClick={() => handleTabClick("messages")}
+        >
+          <div className="w-6 h-6 flex justify-center items-center relative">
+            <div className="w-[18px] h-[18.46px] top-[3px] left-[3px]">
+              <FiMessageSquare />
+            </div>
+            <div className="absolute w-[6px] h-[6px] text-[#DC4A2D] rounded-full top-0 right-0" />
+          </div>
+          <div
+            className={`w-[96px] h-[27px] font-500 text-[20px] leading-[27px]`}
+          >
+            Messages
+          </div>
+        </div>
+
+        <div
+          className={`w-[125px] h-[27px] top-[24px] left-[390px] gap-1 flex cursor-pointer items-center justify-center p-5 transition-all duration-200 ${
+            activeTab === "payments"
+              ? "bg-[#DC4A2D] rounded-full text-[#FFFFFF] w-[155px]"
+              : "bg-transparent text-[#B0B0B0]"
+          }`}
+          onClick={() => handleTabClick("payments")}
+        >
+          <div className="w-6 h-6 flex justify-center items-center">
+            <div className="w-5 h-5 top-[2px] left-[2px]">
+              <PiHandCoins />
+            </div>
+          </div>
+          <div className={`text-[20px] leading-[27px] font-500`}>Payments</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default JobBar;
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,46 +1,49 @@
-import React from 'react'
-import JobBar from './JobBar'
-import { BsBell } from 'react-icons/bs'
-import { IoIosArrowDown } from 'react-icons/io'
+import React from "react";
+import JobBar from "./JobBar";
+import { BsBell } from "react-icons/bs";
+import { IoIosArrowDown } from "react-icons/io";
 
 const NavBar = () => {
   return (
     <>
-        <div className='h-[61px] top-6 left-10 flex justify-around'>
-            <div className='w-[100px] py-[9px] px-[28px] gap-[10px] bg-[#E7E7E7] flex items-center justify-center'>
-                <p className='w-[47px] h-[27px] text-[20px] leading-[27px] font-700 text-[#DC4A2D] top-[20%]'>Logo</p>
-            </div>
-
-            <div className='w-[538px] h-[74px] top-[17px] left-[595px]'>
-                <JobBar/>
-            </div>
-
-            <div className=' w-[134px] h-[60px] p-[10px] gap-4 flex items-center'>
-                <div className='w-[32px] h-[32px] relative'>
-                    <div className='w-[23.3px] h-[25.33px] mt-[2.67px] left-[4.35px] text-[#000000]'>
-                        <BsBell/>
-                    </div>
-
-                    <div className='w-[6px] h-[6px] text-[#DC4A2D] absolute top-0 right-0'/>
-                </div>
-
-                <div className='w-[66px] h-[40px] gap-[6px] flex items-center'>
-                    <div className='w-[40px] h-[40px]'>
-                        <img className='rounded-[60px]' src='https://s3-alpha-sig.figma.com/img/7622/cd8b/0141197fff7d64b94226a8c17be3edce?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l4Y8yiQzPLyWBrt9QqkZn7pR9okvDvC3NgGSV0UGkZuif1-plYp3IhOBV9zw~Y1G4~M~pSheAtnedkSiFzfvqxS1fjySDbX7Zaq8CficHwF10YRGHeGn0zCc9BwvPkqhDzf7tY6lg6lSaAZzUujt5zZ-QEk7WU~qCI~vZo5tpoUmE4KvJmAGIaoHnm5WHe6cg72RJ~NVhCU~1AV9mBVQHKGlohMZM3if9KzQlAq~xSN7O1HoDQP7hyaascss9aWBVsh7SHG~m91DwYa8TfnPrcAY~uBpYzDed8qWVC-x7mkgYM9fRkwiHbxD9rvbSq08mC-dRvKzdt40pdf4TTaFBA__'/>
-                    </div>
-
-                    <div className='w-[20px] h-[20px]'>
-
-                        <div className='w-[10px] h-[5px] top-[7.5px] left-[5px] text-black'>
-                            <IoIosArrowDown/>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
+      <div className="h-[61px] mt-6 ml-10 flex justify-between">
+        <div className="w-[100px] py-[9px] px-[28px] gap-[10px] bg-[#E7E7E7] flex items-center justify-center">
+          <p className="w-[47px] h-[27px] text-[20px] leading-[27px] font-700 text-[#DC4A2D] top-[20%]">
+            Logo
+          </p>
         </div>
-    </>
-  )
-}
 
-export default NavBar
+        <div className="w-[538px] h-[74px] top-[17px] left-[595px]">
+          <JobBar />
+        </div>
+
+        <div className=" w-[134px] h-[60px] p-[10px] gap-4 flex items-center">
+          <div className="w-[32px] h-[32px] relative">
+            <div className="w-[23.3px] h-[25.33px] mt-[2.67px] left-[4.35px] text-[#000000] text-[20px]">
+              <BsBell />
+            </div>
+
+            <div className="w-[6px] h-[6px] text-[#DC4A2D] absolute top-0 right-0" />
+          </div>
+
+          <div className="w-[66px] h-[40px] gap-[6px] flex items-center cursor-pointer">
+            <div className="w-[40px] h-[40px]">
+              <img
+                className="rounded-[60px]"
+                src="https://s3-alpha-sig.figma.com/img/7622/cd8b/0141197fff7d64b94226a8c17be3edce?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l4Y8yiQzPLyWBrt9QqkZn7pR9okvDvC3NgGSV0UGkZuif1-plYp3IhOBV9zw~Y1G4~M~pSheAtnedkSiFzfvqxS1fjySDbX7Zaq8CficHwF10YRGHeGn0zCc9BwvPkqhDzf7tY6lg6lSaAZzUujt5zZ-QEk7WU~qCI~vZo5tpoUmE4KvJmAGIaoHnm5WHe6cg72RJ~NVhCU~1AV9mBVQHKGlohMZM3if9KzQlAq~xSN7O1HoDQP7hyaascss9aWBVsh7SHG~m91DwYa8TfnPrcAY~uBpYzDed8qWVC-x7mkgYM9fRkwiHbxD9rvbSq08mC-dRvKzdt40pdf4TTaFBA__"
+              />
+            </div>
+
+            <div className="w-[20px] h-[20px]">
+              <div className="w-[10px] h-[5px] top-[7.5px] left-[5px] text-black">
+                <IoIosArrowDown />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
