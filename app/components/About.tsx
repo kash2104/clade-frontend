328818e0fec10068f081c77e911922a414a568db
kash2104 <kparikh1104@gmail.com>
2024-06-27 15:53:13 +0530

    about Company

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,16 +3,16 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div className="left-[100px] ml-[100px] mt-10 mb-5 w-[526px] gap-2 flex flex-col top-[379px]">
+      <div className="left-[100px] ml-[93px] mt-8 mb-4 w-[526px] flex flex-col top-[379px]">
         <div className="h-[19px] gap-2">
-          <div className="h-[19px] font-500 text-[14px] leading-[18.9px] text-[rgba(110,109,109,1)]">
+          <div className="h-[19px] font-500 text-[12px] leading-[18.9px] text-[rgba(110,109,109,1)]">
             About the job
           </div>
         </div>
 
-        <div className="w-full font-500 text-[16px] leading-7 text-[rgba(61,61,61,1)]">
-          <div className="flex flex-col gap-4 justify-center">
-            <ol className="list-decimal pl-5">
+        <div className="w-[526px] font-500 text-[14px] leading-7 text-[rgba(61,61,61,1)]">
+          <div className="flex flex-col gap-1 justify-center">
+            <ol className="list-decimal pl-4">
               <li>Handle the UI/UX research design</li>
               <li>
                 Work on researching on latest web applications designs & trends
@@ -24,7 +24,7 @@ const About = () => {
               </li>
             </ol>
 
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col">
               <p>Benefits:</p>
               <ul className="list-disc pl-6">
                 <li>Health insurance</li>
@@ -32,14 +32,14 @@ const About = () => {
               </ul>
             </div>
 
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col">
               <p>Schedule</p>
               <ul className="list-disc pl-6">
                 <li>Day Shift</li>
               </ul>
             </div>
 
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col">
               <p>Supplemental pay types</p>
               <ul className="list-disc pl-6">
                 <li>Performance bonus</li>
