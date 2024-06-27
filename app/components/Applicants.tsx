ff4139b3c43b457fe944374678becc365d61f55d
kash2104 <kparikh1104@gmail.com>
2024-06-27 15:53:29 +0530

    Applicants info

--- app/components/Applicants.tsx
+++ app/components/Applicants.tsx
@@ -9,7 +9,7 @@ const Applicants = ({ text, number, icon }: ApplicantsProps) => {
   return (
     <>
       <div className="w-[336px] h-[42px] flex items-center justify-between">
-        <div className="w-[135px] h-[42px] p-[10px] gap-[10px] flex items-center justify-center">
+        <div className="w-[135px] h-[42px] p-[10px] gap-[5px] flex items-center justify-center">
           <div className=" w-5 h-5">
             <div className="w-[16.67px] h-[15px] top-[2.5px] left-[1.67px] text-[rgba(79,79,79,1)]">
               {icon}
@@ -22,7 +22,7 @@ const Applicants = ({ text, number, icon }: ApplicantsProps) => {
         </div>
 
         <div className="w-[57px] h-[39px] pt-[6px] pb-[6px] pl-[10px] pr-[10px] gap-[10px] flex">
-          <div className="w-[37px] h-[27px] font-600 text-[20px] leading-[27px] text-[rgba(61,61,61,1)]">
+          <div className="w-[37px] h-[27px] font-600 text-[14px] leading-[27px] text-[rgba(61,61,61,1)]">
             {number}
           </div>
         </div>
